37653fd7343a6c3b95b8c8b8592f2ec486e64959
Frederikhome <Frederik.plum@live.dk>
2022-08-27 22:40:42 +0200

    Create StageService.lua

---
+++ src/server/Source/Stage/StageService.lua
@@ -0,0 +1,30 @@
+--[[
+StageService
+2022, 08, 27
+Created by ReelPlum (https://www.roblox.com/users/60083248/profile)
+]]
+
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+
+local knit = require(ReplicatedStorage.Packages.Knit)
+local signal = require(ReplicatedStorage.Packages.Signal)
+local janitor = require(ReplicatedStorage.Packages.Janitor)
+
+local StageService = knit.CreateService({
+  Name = 'StageService', 
+  Client = {},
+  Signals = {
+  }
+})
+
+function StageService:KnitStart()
+  function StageService:UserOwnsStage(user, stage)
+
+  end
+end
+
+function StageService:KnitInit()
+  
+end
+
+return StageService

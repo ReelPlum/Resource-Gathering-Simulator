ce165068335e38f2bd77b25719a2b0d0ad48e47d
Frederikhome <Frederik.plum@live.dk>
2022-08-31 22:40:01 +0200

    Enum fixes (Profile Service did not like numbers)

--- src/shared/common/CustomEnums.lua
+++ src/shared/common/CustomEnums.lua
@@ -6,59 +6,60 @@ Created by ReelPlum (https://www.roblox.com/users/60083248/profile)
 
 local CustomEnums = {
 	["Nodes"] = {
-		["Stone"] = 1,
+		["Stone"] = "1",
 	},
 	["Resources"] = {
-		["Stone"] = 1,
+		["Stone"] = "1",
 	},
 	["NodeRarities"] = {
-		["Normal"] = 1,
+		["Normal"] = "1",
 	},
 	["Tools"] = {
-		["TestTool"] = 1,
+		["TestTool"] = "1",
 	},
 	["ToolTypes"] = {
-		["Pickaxe"] = 1,
+		["Pickaxe"] = "1",
 	},
 	["ToolStates"] = {
-		["Equipped"] = 1,
-		["Stowed"] = 2,
-		["Mining"] = 3,
+		["Equipped"] = "1",
+		["Stowed"] = "2",
+		["Mining"] = "3",
 	},
 	["Currencies"] = {
-		["Coins"] = 1,
+		["Coins"] = "1",
 	},
 	["Enchants"] = {
-		["Fortune"] = 1,
-		["Damage"] = 2,
+		["Fortune"] = "1",
+		["Damage"] = "2",
 	},
-  ["EnchantmentType"] = {
-    ["Pet"] = 1,
-    ["Tool"] = 2,
-  },
+	["EnchantmentType"] = {
+		["Pet"] = "1",
+		["Tool"] = "2",
+	},
 	["Stages"] = {
-		["TestStage"] = 1,
+		["TestStage"] = "1",
 	},
 	["Worlds"] = {
-		["TestWorld"] = 1,
+		["TestWorld"] = "1",
 	},
 	["PlayerStats"] = {
-		["DestroyedNodes"] = 1,
+		["DestroyedNodes"] = "1",
+		["Playtime"] = "2",
 	},
 	["Boosts"] = {
-		["MoreResources"] = 1,
+		["MoreResources"] = "1",
 	},
-  ["BoostTypes"] = {
-    ["Drops"] = 1,
-    ["Damage"] = 2,
-  },
-  ["Pets"] = {
-    ["TestPet"] = 1,
-  },
-  ["PetStats"] = {
-    ["WalkSpeed"] = 1,
-    ["Damage"] = 1,
-  },
+	["BoostTypes"] = {
+		["Drops"] = "1",
+		["Damage"] = "2",
+	},
+	["Pets"] = {
+		["TestPet"] = "1",
+	},
+	["PetStats"] = {
+		["WalkSpeed"] = "1",
+		["Damage"] = "1",
+	},
 }
 
 return CustomEnums
